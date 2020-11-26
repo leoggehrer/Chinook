523ae3022491ec5c34f712f1ae03b50970d3ab89
Gerhard <g.gehrer@htl-leonding.ac.at>
2020-11-26 11:07:23 +0100

    Added build events do copy csv-files.

---
+++ Chinook.WebApi/Controllers/MarketingStatisticsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Chinook.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MarketingStatisticsController : ControllerBase
+    {
+        [HttpGet("/api/[controller]/TrackStatistic")]
+        public Contracts.Report.Marketing.ITrackStatistic GetTrackStatistic()
+        {
+            return Report.MarketingReports.GetTrackStatistic();
+        }
+
+        [HttpGet("/api/[controller]/GenreStatistics")]
+        public IEnumerable<Contracts.Report.Marketing.IGenreStatistic> GetGenreStatitics()
+        {
+            return Report.MarketingReports.GetGenreStatistics();
+        }
+    }
+}
